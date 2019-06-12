56eb63ad250f351c47c54d6cd25a1b0a18c2e9f9
Assstarte <admin@ezic.io>
2019-06-12 20:18:56 +0300

    Set u DB & .env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+
+const port = process.env.SRV_PORT || 3030;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const srv = await NestFactory.create(AppModule);
+  await srv.listen(port);
+  Logger.log(`Server is running on ${port} port.`, 'Bootstrap');
 }
 bootstrap();
